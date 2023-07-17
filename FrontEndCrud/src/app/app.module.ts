bd889c9dcb6277e55d6698e0e597d1b422dab304
FrancoFelg <64760354+FrancoFelg@users.noreply.github.com>
2023-07-16 22:30:01 -0300

    Imports de Angular

--- FrontEndCrud/src/app/app.module.ts
+++ FrontEndCrud/src/app/app.module.ts
@@ -4,13 +4,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {ReactiveFormsModule} from '@angular/forms';//formularios reactivos
+import {HttpClientModule} from '@angular/common/http';//peticiones http
+
+//Importar componentes Material Design
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+//Alertas
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import {MatDialogModule} from '@angular/material/dialog';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatGridListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
