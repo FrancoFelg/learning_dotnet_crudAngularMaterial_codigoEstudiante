be5e251fde88a3808f3e0ef2b3c378d0185f571d
FrancoFelg <64760354+FrancoFelg@users.noreply.github.com>
2023-07-25 09:08:40 -0300

    Video 04 Terminado

--- APISistemaVenta/SistemaVenta.DTO/DetalleVentaDTO.cs
+++ APISistemaVenta/SistemaVenta.DTO/DetalleVentaDTO.cs
@@ -9,7 +9,7 @@ namespace SistemaVenta.DTO
     public class DetalleVentaDTO
     {
         public int? IdProducto { get; set; }
-        public string? DescripcionProductos { get; set;}
+        public string? DescripcionProducto { get; set;}
         public int? Cantidad { get; set;}
         public string? PrecioTexto { get; set;}
         public string? TotalTexto { get; set;}
--- APISistemaVenta/SistemaVenta.DTO/VentaDTO.cs
+++ APISistemaVenta/SistemaVenta.DTO/VentaDTO.cs
@@ -11,7 +11,7 @@ namespace SistemaVenta.DTO
         public int IdVenta { get; set; }        
         public string? NumeroDocumento { get; set; }
         public string? TipoPago { get; set; }
-        public string? Total { get; set; }
+        public string? TotalTexto { get; set; }
         public string? FechaRegistro { get; set; }
         public virtual ICollection<DetalleVentaDTO> DetalleVenta { get; set; }
     }
